88cf0a4926784accf15395573142941f4cd52c69
James Wakefield <james.wakefield@sjc.ox.ac.uk>
2024-11-02 16:07:24 +0000

    commiting

--- plot_data_and_model.R
+++ plot_data_and_model.R
@@ -1,6 +1,6 @@
 #Script to plot data and model
 
-growth_data <- read.csv("???")
+growth_data <- read.csv("experiment.csv")
 
 logistic_fun <- function(t) {
   
@@ -10,13 +10,13 @@ logistic_fun <- function(t) {
   
 }
 
-N0 <- ??? #
+N0 <-exp(7.1855445)  #intercept of model 1 (then back transformed)
   
-r <- ??? #
+r <-0.0095218 #gradient of model 1
   
-K <- ??? #
+K <-exp(-6.00e+10) #intercept of model 2 (then back transformed)
 
-ggplot(aes(???,???), data = growth_data) +
+ggplot(aes(t, N), data = growth_data) +
   
   geom_function(fun=logistic_fun, colour="red") +
   
